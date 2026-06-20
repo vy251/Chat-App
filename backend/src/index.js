bfb03e4a968d7e302439b138f460f28ceef964b7
vivek-yadav41301 <vivekyadav04919@gmail.com>
2026-06-20 15:26:19 +0530

    i add front end host  url in cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,9 +18,14 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://chatting-app-2-front.onrender.com",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
