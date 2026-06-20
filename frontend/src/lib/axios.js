bddf1d0321bde3a351ed83a890393444c5f38641
vivek-yadav41301 <vivekyadav04919@gmail.com>
2026-06-20 13:54:52 +0530

    addd env file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from "axios";
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_DEV_URL
+    : import.meta.env.VITE_PROD_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: baseURL + "/api",
   withCredentials: true,
-});
+}); 
