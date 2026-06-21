d9f90754e7bd6f588656f033692490aeefedb84b
Vishal Yadav <vy1370382@gmail.com>
2026-06-21 14:20:03 +0530

    fixed issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,21 +24,13 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://chatting-app-2-front.onrender.com",
+      "https://chat-app-frontend-k5z4.onrender.com",
     ],
     credentials: true,
   })
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
